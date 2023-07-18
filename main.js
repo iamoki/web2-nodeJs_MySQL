bf183efcd41f67f7957284967423de1a947c5133
anoki <pureaoh@gmail.com>
2023-07-18 22:56:46 +0900

    ✅ mysql로 리스트 불러오고, create기능 만들기

--- main.js
+++ main.js
@@ -33,35 +33,37 @@ var app = http.createServer(function(request,response){
           response.end(html);
         });
       } else {
-        fs.readdir('./data', function(error, filelist) {
-          var filteredId = path.parse(queryData.id).base;
-          fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
-            var title = queryData.id;
-            var sanitizeTitle = sanitizeHtml(title);
-            var sanitizeDescription = sanitizeHtml(description, {
-              allowedTags: ['h1']
-            });
-            var list = template.list(filelist);
+        db.query(`SELECT * FROM topic`, function(error,topics) {
+          if(error) {
+            throw error;
+          }
+          db.query(`SELECT * FROM topic WHERE id=?`, [queryData.id], function(error2, topic) {
+            if(error2) {
+              throw error2;
+            }
+            var title = topic[0].title;
+            var description = topic[0].description;
+            var list = template.list(topics);
             var html = template.html(title, list, 
-              `<h2>${sanitizeTitle}</h2>${sanitizeDescription}`,
+              `<h2>${title}</h2>${description}`,
               `<a href="/create"}>create</a> 
-                <a href="/update?id=${sanitizeTitle}">update</a>
+                <a href="/update?id=${queryData.id}">update</a>
                 <form action="delete_process" method="post">
-                  <input type="hidden" name="id" value="${sanitizeTitle}"/>
+                  <input type="hidden" name="id" value="${queryData.id}"/>
                   <input type="submit" value="delete"/>
-                </form>
-              `
+                </form>`
             );
             response.writeHead(200);
             response.end(html);
-          });
+          })
         });
       }
     } else if(pathname === '/create') {
-      fs.readdir('./data', function(error, filelist) {
-        var title = 'WEB - create';
-        var list = template.list(filelist);
-        var html = template.html(title, list, `
+      db.query(`SELECT * FROM topic`, function(error,topics) {
+        var title = 'Create';
+        var list = template.list(topics);
+        var html = template.html(title, list, 
+          `
           <form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
@@ -71,7 +73,9 @@ var app = http.createServer(function(request,response){
                 <input type="submit">
             </p>
           </form>
-        `, '');
+          `,
+          `<a href="/create"}>create</a>`
+        );
         response.writeHead(200);
         response.end(html);
       });
@@ -82,12 +86,18 @@ var app = http.createServer(function(request,response){
       });
       request.on('end', function() {
         var post = qs.parse(body);
-        var title = post.title;
-        var desc = post.desc;
-        fs.writeFile(`data/${title}`, desc, 'utf8', function(err) {
-          response.writeHead(302, {Location: `/?id=${title}`});
-          response.end();
-        });
+        db.query(`
+          INSERT INTO topic (title, description, created, author_id) 
+          VALUES(?, ?, NOW(), 1)`,
+          [post.title, post.desc, 1],
+          function(error, result) {
+              if(error) {
+                throw error;
+              }
+              response.writeHead(302, {Location: `/?id=${result.insertId}`});
+              response.end();
+            }
+          )
       });
     } else if(pathname === '/update') {
       fs.readdir('./data', function(error, filelist) {
